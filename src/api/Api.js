f1aa68e28f21834ae441ea15df9d8f3b7179928c
cherryctr <chrryctr1509@gmail.com>
2023-05-07 03:20:54 +0700

    first commit

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://localhost:8000/api'
+    // baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://admintoko.belajarseoweb.com/api'
 })
 
 export default Api
