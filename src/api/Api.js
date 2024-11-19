099ba55ca0d30abea8e9c2180f4b2a07a2073f67
Cherry citra <chrryctr1509@gmail.com>
2024-11-19 18:01:26 +0700

    fix api

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const Api = axios.create({
     //set default endpoint API
     // baseURL: 'http://localhost:8000/api'
-    baseURL: 'https://admintoko.belajarseoweb.com/api'
+    baseURL: 'https://adminshop.punyaasset.com//api'
 })
 
 export default Api
